60a5238436d454eb8f28201bcc9835be4925d9b6
charlesgarmendia <cash19617309@gmail.com>
2026-01-29 20:52:22 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = 'total-kpop-v1';
+const CACHE_NAME = 'total-kpop-v2'; // Cambiamos a v2 para forzar actualización
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.png',
-  './privacy.html'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png',
+  '/privacy.html'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Esto obliga al nuevo Service Worker a activarse ya mismo
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-
 
 
 
